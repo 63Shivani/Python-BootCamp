2807d4a4b5d2d46d6b270cb8e9360bc12cd064ef
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-03-23 19:27:34 +0530

    Create numbers.py

---
+++ Shivani/numbers.py
@@ -0,0 +1,8 @@
+a=int(input("enter 1st number\n"))
+b=int(input("enter 2nd number\n"))
+c=int(input("enter 3rd number\n"))
+d=int(input("enter 4th number\n"))
+total=a+b+c+d
+average=(a+b+c+d)/4
+print("total of four number",total)
+print("average of four number",average)
