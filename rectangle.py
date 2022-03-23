c36e0f9b77c4c4eb0b25d21247fe9616288672f0
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-03-23 19:00:17 +0530

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+l=int(input("enter length"))
+b=int(input("enter breath\n"))
+area=l*b
+print(area)
+
+l=int(input("enter length"))
+b=int(input("enter breath\n"))
+perimeter=2*(l+b)
+print(perimeter)
