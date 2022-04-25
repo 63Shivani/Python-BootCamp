df58b4039ea2320785969c51d8a77dd6d07ca4f2
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-04-25 20:38:15 +0530

    Create month.py

---
+++ Shivani/month.py
@@ -0,0 +1,27 @@
+month = int(input("enter number between 1 to 12\n"))
+if(month == 1):
+    print("January, 31 Days")
+elif(month == 2):
+    print("February, 28 Days")
+elif(month == 3):
+    print("March, 31 Days")
+elif(month == 4):
+    print("April, 30 Days")
+elif(month == 5):
+    print("May, 31 days")
+elif(month == 6):
+    print("June, 30 Days")
+elif(month == 7):
+    print("july, 31 Days")
+elif(month == 8):
+    print("August, 31 Days")
+elif(month == 9):
+    print("September, 30 Days")
+elif(month == 10):
+    print("October, 31 Days")
+elif(month == 11):
+    print("November, 30 Days")
+elif(month == 12):
+    print("December, 31 Days")
+else:
+    print("No Month")
