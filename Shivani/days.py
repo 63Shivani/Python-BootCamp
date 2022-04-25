d76d2b2b86b8416af83e2ff3339db4101b9d84d3
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-04-25 20:29:20 +0530

    Create days.py

---
+++ Shivani/days.py
@@ -0,0 +1,17 @@
+day = int(input("enter number between 1 to 7\n"))
+if(day == 1):
+    print("Sunday")
+elif(day == 2):
+    print("Monday")
+elif(day == 3):
+    print("Tuesday")
+elif(day == 4):
+    print("Wednesday")
+elif(day == 5):
+    print("Thrusday")
+elif(day == 6):
+    print("Friday")
+elif(day == 7):
+    print("Saturday")
+else:
+    print("No day")
