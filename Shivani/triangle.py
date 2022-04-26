3a2810ee820536d781bb584e8f073b068fb779a0
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-04-26 13:43:35 +0530

    Create triangle.py

---
+++ Shivani/triangle.py
@@ -0,0 +1,7 @@
+x = int(input("enter first side\n"))
+y = int(input("enter second side\n"))
+z = int(input("enter third side\n"))
+if(x==y==z):
+    print("equilateral triangle")
+else:
+    print("scalene triangle")
