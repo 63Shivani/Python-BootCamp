b6f7e42eed26a4eeb2c9074d7df0042ded4efe96
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-04-26 13:33:37 +0530

    Create marks.py

---
+++ Shivani/marks.py
@@ -0,0 +1,9 @@
+x = int(input("enter percentage\n"))
+if(x>=65):
+    print("Excellent")
+elif(x>=55 and x<65):
+    print("Good")
+elif(x>=40 and x<55):
+    print("Fair")
+else:
+    print("Failed")
